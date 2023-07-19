cd9f33b3bfc9ae2449b97fc8b6187809c2fe586d
Muchesia <muchesiawallace@gmail.com>
2023-07-19 16:47:32 +0300

    Illustrate function callback

---
+++ function_pointers/callback.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*A - print Hello
+**/
+
+void A(void)
+{
+	printf("Hello\n");
+}
+
+/**
+*B - callback function
+*@ptr: function pointer to callback
+**/
+void B(void (*ptr)())
+{
+	ptr();
+}
+
+/**
+*main - control execution
+*Return: 0 for success, non-zero for failure
+**/
+int main(void)
+{
+	void (*p)() = A;
+
+	B(p);
+	return (0);
+}
