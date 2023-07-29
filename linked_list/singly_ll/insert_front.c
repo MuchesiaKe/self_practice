975686ab002053a0dd5e4170480569be4786c144
Muchesia <muchesiawallace@gmail.com>
2023-07-29 05:18:14 +0300

    Insert node at the front

--- linked_list/singly_ll/insert_front.c
+++ linked_list/singly_ll/insert_front.c
@@ -1,3 +1,5 @@
+#include "single.h"
+
 /**
 *push - inserts new node on the front of the ll
 *@head_ref: reference to the head
@@ -6,10 +8,46 @@
 
 void push(struct Node **head_ref, int new_data)
 {
-	struct Node new_node;
+	struct Node *new_node;
 
 	new_node = malloc(sizeof(new_node));
 	new_node->data = new_data;
 	new_node->next = *head_ref;
 	*head_ref = new_node;
+}
+
+/**
+*printList - print content of linked list starting from head
+*@node: reference to head
+**/
+void printList(struct Node *node)
+{
+	while (node != NULL)
+	{
+		printf(" %d", node->data);
+		node = node->next;
+	}
+	printf("\n");
+}
+
+/**
+*main - driver code
+*Return: 0 for success, non-zero for failure
+**/
+
+int main(void)
+{
+	struct Node *head = NULL;
+
+	push(&head, 6);
+	push(&head, 5);
+	push(&head, 4);
+	push(&head, 3);
+	push(&head, 2);
+	printf("Created Linked list is: ");
+	printList(head);
+	push(&head, 1);
+	printf("After inserting 1 at front: ");
+	printList(head);
+	return (0);
 }
---
+++ linked_list/singly_ll/single.h
@@ -0,0 +1,16 @@
+#ifndef SINGLE_H
+#define SINGLE_H
+/**
+*Node : struct for the linked list operations
+*@data: value for the struct node
+*@next: pointer to the next node
+**/
+	struct Node
+	{
+		int data;
+		struct Node *next;
+	};
+
+	#include <stdlib.h>
+	#include <stdio.h>
+#endif
