551deb01985fbe417db46baa52f2e1a63d6135bc
Muchesia <muchesiawallace@gmail.com>
2023-09-10 14:25:32 +0300

    Read line

--- shell/getline.c
+++ shell/getline.c
@@ -9,18 +9,11 @@
 int main(void)
 {
 	char *buffer;
-	size_t buffsize = 32;
-	size_t characters;
+	size_t buffsize;
 
-	buffer = (char *)malloc(buffsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer\n");
-		exit(1);
-	}
-	printf("Type something: ");
-	characters = getline(&buffer, &buffsize, stdin);
-	printf("%lu characters were read.\n", characters);
-	printf("You typed: '%s'\n", buffer);
+	buffer = malloc(sizeof(buffer));
+	printf("$ ");
+	getline(&buffer, &buffsize, stdin);
+	printf("%s", buffer);
 	return (0);
 }
